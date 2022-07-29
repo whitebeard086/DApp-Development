aeb492410f758e12f61520f5532e44bd11cc2875
The Exorcist <whitebeard086@gmail.com>
2022-07-29 15:29:15 +0100

    Constructor for the will

--- practice/will.sol
+++ practice/will.sol
@@ -4,4 +4,10 @@ contract will {
     address owner;
     uint fortune;
     bool deceased;
+
+    constructor() payable public {
+        owner = msg.sender; // msg.sender represents address that is being called
+        fortune = msg.value; // msg.value tells us how much is being sent
+        deceased = false;
+    }
 }
