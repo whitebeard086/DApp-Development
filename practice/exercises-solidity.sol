da38f9936890738af156a67347dba955490c37fc
The Exorcist <whitebeard086@gmail.com>
2022-07-29 10:30:47 +0100

    Variables in solidity

--- practice/exercises-solidity.sol
+++ practice/exercises-solidity.sol
@@ -1,3 +1,19 @@
 // this is where our code goes
 
 pragma solidity >=0.7.0 <0.9.0;
+
+// Create a contract that can store data and return the data back
+
+// Be able to do the following
+
+// 1. Receive information, 2. Store information and 3. Return the information back
+
+// A smart contract in the sense of Solidity is a collection of code (its functions) and data (its state) that resides at a specific address on the Ethereum blockchain.
+
+contract simpleStorage {
+    // write all the code inside here - functions and its state
+
+    uint storeData = "28"
+    string names = "Alex"
+    bool switchON = true
+}
