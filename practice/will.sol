6117d9c0a469db2c6ea7a064abbca7edd65accbe
The Exorcist <whitebeard086@gmail.com>
2022-07-30 20:31:01 +0100

    Modifiers in solidity

--- practice/will.sol
+++ practice/will.sol
@@ -10,4 +10,18 @@ contract will {
         fortune = msg.value; // msg.value tells us how much is being sent
         deceased = false;
     }
+
+    // Modifiers are add-ons for functions that can create additional logic for them
+
+    // create modifier so the only person who can call the contract is the owner
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // create modifier so that we can allocate funds if friend's gramps is deceased
+    modifier grampsDeceased {
+        require(deceased == true);
+        _;
+    }
 }
