9aa82091639d15fac52e448b0b66bfdc5b387b65
The Exorcist <whitebeard086@gmail.com>
2022-08-01 18:40:32 +0100

    Send tokens function

--- subCurrency.sol
+++ subCurrency.sol
@@ -7,6 +7,8 @@ contract Coin {
     address public minter;
     mapping(address => uint) public balances;
 
+    event Sent(address from, address to, uint amount);
+
     // constructor functions only run when the contract is deployed
     constructor() {
         minter = msg.sender;
@@ -16,6 +18,19 @@ contract Coin {
     // only the owner should be able to send these coins
     function mint(address receiver, uint amount) public {
         require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+    // send any amount of coins to an existing address
+    error insufficientBalance(uint requested, uint available);
+
+    function send(address receiver, uint amount) public {
+        if(amount > balances[msg.sender])
+        revert insufficientBalance({
+            requested: amount,
+            available: balances[msg.sender]
+        });
+        balances[msg.sender] -= amount;
         balances[receiver] += amount;
     }
 }
