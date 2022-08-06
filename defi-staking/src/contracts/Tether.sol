90580620e614b8f66dd47092564bf68b64723dd7
The Exorcist <whitebeard086@gmail.com>
2022-08-06 19:28:36 +0100

    Tether smart contract shell

---
+++ defi-staking/src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 10000000000000000000000; // 1 million tokens 
+}
