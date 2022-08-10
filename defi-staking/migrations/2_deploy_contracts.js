3781a3357f006d6a1c9b3b268f654d7e4dba66fc
The Exorcist <whitebeard086@gmail.com>
2022-08-10 09:17:13 +0100

    Smart contract transaction and interactions between each other

--- defi-staking/migrations/2_deploy_contracts.js
+++ defi-staking/migrations/2_deploy_contracts.js
@@ -3,13 +3,22 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   // Deploy Tether contract
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
 
   // Deploy Reward contract
   await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
 
   // Deploy DecentralBank contract
   await deployer.deploy(DecentralBank);
+  const decentralBank = await DecentralBank.deployed()
+
+  // Transfer all RWD tokens to DecentralBank
+  await rwd.transfer(DecentralBank.address, "1000000000000000000000000");
+
+  // Distribute 100 Tether tokens to investors
+  await tether.transfer(accounts[1], "100000000000000000000");
 };
