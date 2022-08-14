04d764d657db78a07317a5e9242780ebbe372344
The Exorcist <whitebeard086@gmail.com>
2022-08-14 20:19:57 +0100

    debugging and setting up solidity truffle tests with chai

---
+++ defi-staking/tests/decentralBank.tests.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract ("DecentralBank", accounts => {
+    // All of the codes for testing goes here
+})
