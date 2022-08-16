242d69aa5b7c38b3eed44b9439b61c3d31b99b64
The Exorcist <whitebeard086@gmail.com>
2022-08-16 08:43:50 +0100

    Staking logic

--- defi-staking/src/contracts/DecentralBank.sol
+++ defi-staking/src/contracts/DecentralBank.sol
@@ -15,7 +15,7 @@ contract DecentralBank {
 
     mapping (address => uint) public stakingBalance;
     mapping (address => bool) public hasStaked;
-    mapping (address => bool) public isStaked;
+    mapping (address => bool) public isStaking;
 
     constructor(RWD _rwd, Tether _tether) {
         rwd = _rwd;
@@ -24,10 +24,21 @@ contract DecentralBank {
 
     // Staking function
     function depositTokens(uint _amount) public {
+        // Require staking amount to be greater than 0
+        require(_amount > 0, "Amount must be greater than 0");
         // Transfer Tether tokens to this contract address for staking
         tether.transferFrom(msg.sender, address(this), _amount);
 
         // Update the staking balance of the sender
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Check if the sender has staked before
+        if (!hasStaked) {
+            stakers.push[msg.sender];
+        }
+
+        // Update the staked status of the sender
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
