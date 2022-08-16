6de82c85facaa83de763873994994f0d1f8431b3
The Exorcist <whitebeard086@gmail.com>
2022-08-16 08:28:06 +0100

    Deposit tokens functionality

--- defi-staking/src/contracts/DecentralBank.sol
+++ defi-staking/src/contracts/DecentralBank.sol
@@ -11,8 +11,23 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping (address => uint) public stakingBalance;
+    mapping (address => bool) public hasStaked;
+    mapping (address => bool) public isStaked;
+
     constructor(RWD _rwd, Tether _tether) {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    // Staking function
+    function depositTokens(uint _amount) public {
+        // Transfer Tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Update the staking balance of the sender
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
     }
 }
